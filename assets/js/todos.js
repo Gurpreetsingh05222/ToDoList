e359fcee8c2476f86265acc3994becdd8b52800a
unknown <gurpreetsingh05222@gmail.com>
2020-03-15 08:08:50 +0530

    deleting todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,12 @@
+//Check off todos
 $("li").click(function(){
     $(this).toggleClass("completed");
+});
+
+//Click on x to delete
+$("span").click(function(e){
+    $(this).parent().fadeOut(function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
 });
